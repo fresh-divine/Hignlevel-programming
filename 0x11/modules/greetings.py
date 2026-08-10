b23a09eeebb8c458fbb3d11f8d23e2be046058ec
fresh-divine <fresh.divine07@gmail.com>
2026-08-10 14:10:56 +0100

    Add validate_email() to validator module

---
+++ 0x11/modules/greetings.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+def greet(name):
+    return f"Hello and good day {name}!"
+name=input("what is your name: ")
+print(greet(name))
---
+++ 0x11/modules/main_greeting.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+def greet(name):
+    return f"Hello and good day {name}"
+
+name=input("what is your name ")
+if __name__ == '__main__':
+    print( f"Hello and good day {name}")
+    
